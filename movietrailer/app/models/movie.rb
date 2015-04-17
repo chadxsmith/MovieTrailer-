c4eab86f5c8c087969beb624e847044c1fe2385d
Chad Smith <chadxsmith@ymail.com>
2015-04-17 12:35:16 -0400

    w/ seeds

--- movietrailer/app/models/movie.rb
+++ movietrailer/app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
+  has_many :actors
+  has_many :trailers
 
 
 end
--- movietrailer/db/seeds.rb
+++ movietrailer/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movie = Movie.create(title:'X', year:2012, plot:"A Love Story", poster_url:"http://ia.media-imdb.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_SY298_CR2,0,201,298_AL_.jpg")
+movie.trailers.create(title:'X', embedded_url:"Test Embedded")
+movie.actors.create(name:'John Doe', photo_url:"http://ia.media-imdb.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_SX214_CR0,0,214,317_AL_.jpg")
